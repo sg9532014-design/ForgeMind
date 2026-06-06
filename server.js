42abbd88ed32592b6cd27d7d42981bec5c46bdb4
sg9532014-design <sg9532014@gmail.com>
2026-06-05 21:25:18 -0600

    Update CORS configuration in server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 3000;
 const client = new MercadoPagoConfig({ accessToken: process.env.MERCADOPAGO_TOKEN });
 
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
+
 app.use(bodyParser.json());
 app.use(express.static('.')); // Sirve tus archivos index.html y script.js
 
